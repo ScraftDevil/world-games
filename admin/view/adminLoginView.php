04887008640f2a3d8fe8996904a5c4083233c5d0
ScraftDevil <davidchicamolina@gmail.com>
2016-05-09 17:32:03 +0200

    Admin Login View
    Admin Login View

---
+++ admin/view/adminLoginView.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel - World Games</title>
+	<meta charset="utf-8"/>
+	<style type="text/css" src="css/adminLogin.css"></style>
+	<script type="text/javascript" src="js/adminLogin.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="card card-container">
+			<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+			<p id="profile-name" class="profile-name-card"></p>
+			<form class="form-signin">
+				<span id="reauth-email" class="reauth-email"></span>
+				<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+				<div id="remember" class="checkbox">
+					<label>
+						<input type="checkbox" value="remember-me"> Remember me
+					</label>
+				</div>
+				<button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+			</form>
+			<a href="#" class="forgot-password">Forgot the password?</a>
+		</div>
+	</div>
+</body>
+</html>
---
+++ admin/view/js/adminLogin.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+    loadProfile();
+});
+
+function getLocalProfile(callback){
+    var profileImgSrc      = localStorage.getItem("PROFILE_IMG_SRC");
+    var profileName        = localStorage.getItem("PROFILE_NAME");
+    var profileReAuthEmail = localStorage.getItem("PROFILE_REAUTH_EMAIL");
+
+    if(profileName !== null && profileReAuthEmail !== null && profileImgSrc !== null) {
+        callback(profileImgSrc, profileName, profileReAuthEmail);
+    }
+}
+
+function loadProfile() {
+    if(!supportsHTML5Storage()) {
+        return false; 
+    }
+    getLocalProfile(function(profileImgSrc, profileName, profileReAuthEmail) {
+        $("#profile-img").attr("src",profileImgSrc);
+        $("#profile-name").html(profileName);
+        $("#reauth-email").html(profileReAuthEmail);
+        $("#inputEmail").hide();
+        $("#remember").hide();
+    });
+}
+
+function supportsHTML5Storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+function testLocalStorageData() {
+    if(!supportsHTML5Storage()) { return false; }
+    localStorage.setItem("PROFILE_IMG_SRC", "//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" );
+    localStorage.setItem("PROFILE_NAME", "César Izquierdo Tello");
+    localStorage.setItem("PROFILE_REAUTH_EMAIL", "oneaccount@gmail.com");
+}
