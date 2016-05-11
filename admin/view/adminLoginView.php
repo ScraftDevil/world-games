b861a1599cee20d142dfaf259092cacb8202cbc5
ScraftDevil <davidchicamolina@gmail.com>
2016-05-11 16:20:44 +0200

    .

--- admin/view/adminLoginView.php
+++ admin/view/adminLoginView.php
@@ -4,21 +4,23 @@
 <body>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 card card-container">
-				<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-				<p id="profile-name" class="profile-name-card"></p>
-				<form class="form-signin">
-					<span id="reauth-email" class="reauth-email"></span>
-					<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-					<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-					<div id="remember" class="checkbox">
-						<label>
-							<input type="checkbox" value="remember-me"> Remember me
-						</label>
-					</div>
-					<button class="btn btn-lg btn-primary btn-block btn-signin login" type="submit">Sign in</button>
-					<a href="#" class="forgot-password">Forgot the password?</a>
-				</form>
+			<div class="col-md-offset-4 col-md-4">
+				<div class="card card-container">
+					<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+					<p id="profile-name" class="profile-name-card"></p>
+					<form class="form-signin">
+						<span id="reauth-email" class="reauth-email"></span>
+						<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+						<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+						<div id="remember" class="checkbox">
+							<label>
+								<input type="checkbox" value="remember-me"> Remember me
+							</label>
+						</div>
+						<button class="btn btn-lg btn-primary btn-block btn-signin login" type="submit">Sign in</button>
+						<a href="#" class="forgot-password">Forgot the password?</a>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
