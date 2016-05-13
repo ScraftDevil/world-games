3ce5cb563e326f827b0e51068dd0cb9717a79c33
darkhc <cristian14rap@gmail.com>
2016-05-13 18:58:15 +0200

    NAV

--- view/sections/nav.php
+++ view/sections/nav.php
@@ -12,9 +12,13 @@
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active nav-item"><a href="home.php">Inicio</a></li>
-                    <li class="nav-item"><a href="games.php">Juegos</a></li>
-                    <li class="nav-item"><a href="login.php">Entrar</a></li>
+                    <?php
+                    $page = str_replace(".php", "", basename($_SERVER['PHP_SELF']));
+                    echo $page;
+                    ?>
+                    <li class="<?php if ($page=="home") { echo 'active ';}?>nav-item"><a href="home.php">Inicio</a></li>
+                    <li class="<?php if ($page=="games") { echo 'active ';}?>nav-item"><a href="games.php">Juegos</a></li>
+                    <li class="<?php if ($page=="login") { echo 'active ';}?>nav-item"><a href="login.php">Entrar</a></li>
                 </ul>
                 <div class="col-md-4">
                     <form class="navbar-form" role="search" style="padding-top: 13px">
