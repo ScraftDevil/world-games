b408579a994980ac1e2fd1220f0b222b3ce49f66
ignaciocast <ignaciocastanolopez@gmail.com>
2016-05-14 18:43:29 +0200

    faltaba incluir mi archivo jquery del perfil
    segunda vez que tengo que incluir la linia..

--- view/sections/footer.php
+++ view/sections/footer.php
@@ -38,6 +38,7 @@
 <script type="text/javascript" src="js/jquery.counterup.min.js"></script>
 <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
 <script type="text/javascript" src="js/bskit-scripts.js"></script>
+<script type="text/javascript" src="js/userProfile.js"></script>
 <!--[if lt IE 9]><script src="js/html5shiv.js"></script><script src="js/respond.min.js"></script><![endif]-->
 <!--    SEARCH  -->
 <script type="text/javascript">
