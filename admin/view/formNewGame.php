420f184293ebbc3243c17c01d95c082f21aa29a7
darkhc <cristian14rap@gmail.com>
2016-05-18 17:05:53 +0200

    Renames

--- admin/view/formNewGame.php
+++ admin/view/formNewGame.php
@@ -1,7 +1,7 @@
 
 <?php
 session_start();
-include("../controller/controlerselectoffer.php");
+include("../controller/showOffersController.php");
 
  ?>
 
--- controller/controlerinsergame.php
+++ controller/controlerinsergame.php
@@ -1,19 +1,13 @@
 <?php
-
 require_once("../model/autoload.php");
-
 session_start();
-
 if (isset($_REQUEST['add'])) {
- 
         $shop = unserialize($_SESSION['shop']);
         try {
             $shop->addGame($_REQUEST['title'], $_REQUEST['precio']);
-            
         } catch (Exception $exc) {
             printErrorConnectionDb();
         }
-        $_SESSION['shop'] = serialize($shop);        
-    
+        $_SESSION['shop'] = serialize($shop);
 }
 ?>
