39088371cf8079b478da7b9036e7469706cf8e93
ignaciocast <ignaciocastanolopez@gmail.com>
2016-05-19 17:37:47 +0200

    perfil de registrador

--- view/registeredProfileView.php
+++ view/registeredProfileView.php
@@ -7,8 +7,9 @@
 
 include("sections/head.php");
 
-// controlador del perfil de usuario registrado
-include("../controller/registeredProfileController.php");
+// controladores del perfil de usuario registrado
+include("../controller/getRegisteredInfoController.php");
+//include("../controller/updateRegisteredController.php");
 
  //array que contiene los datos del usuario segun el id obtenido después de hacer login
 $registered = getRegisteredInfo($_SESSION['user_id']);
@@ -44,33 +45,36 @@ $_SESSION['registered'] = serialize($registeredObject);?>
                                     Datos Personales
                                 </div>
 
+                                <!-- Error de que no se puede actualizar por falta de datos o datos incorrectos -->
+                                <div id="general-error"></div>
+
                                 <!-- Formulario para poder editar los datos del usuario (obtiene los datos de la BBDD) -->
-                                <form id="profileForm" action="../controller/registeredProfileController.php" method="POST">
+                                <form id="profileForm">
                                     <div class="form-group col-lg-12">
                                         <label class="col-xs-12 col-sm-4 col-md-4 col-lg-4 control-label attributeText">Correo Eletrónico</label>
-                                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-5 inputAttribute">
-                                            <input class="form-control" type="text" name="email" placeholder="micorreo@ejemplo.com" value="<?php echo $registeredObject->getEmail();?>">
+                                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 inputAttribute">
+                                            <input id="email" class="form-control" type="text" name="email" placeholder="micorreo@ejemplo.com" value="<?php echo $registeredObject->getEmail();?>">
                                         </div>
                                     </div>
 
                                     <div class="form-group col-lg-12">
                                         <label class="col-xs-12 col-sm-4 col-md-4 col-lg-4 control-label attributeText">Fecha de Nacimiento:</label>
-                                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-5 inputAttribute">
+                                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 inputAttribute">
                                             <input id="calendar" class="form-control" type="text" name="birthdate" placeholder="AAAA-MM-DD" value="<?php echo $registeredObject->getBirthDate();?>">
                                         </div>
                                     </div>
 
                                     <div class="form-group col-lg-12">
                                         <label class="col-xs-12 col-sm-4 col-md-4 col-lg-4 control-label attributeText">Cuenta PayPal:</label>
-                                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-5 inputAttribute">
-                                            <input class="form-control" type="text" name="paypal" placeholder="micorreoPayPal@ejemplo.com" value="<?php echo $registeredObject->getPaypalAccount();?>">
+                                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 inputAttribute">
+                                            <input id="paypal" class="form-control" type="text" name="paypal" placeholder="micorreoPayPal@ejemplo.com" value="<?php echo $registeredObject->getPaypalAccount();?>">
                                         </div>
                                     </div>
 
                                     <div class="form-group col-lg-12">
                                         <label class="col-xs-12 col-sm-4 col-md-4 col-lg-4 control-label attributeText">País:</label>
                                         <div class="col-xs-12 col-sm-8 col-md-8 col-lg-5 inputAttribute">
-                                            <select name="country">
+                                            <select id="country" name="country">
                                                 <?php getCountriesList($registeredObject->getCountry()); ?>
                                             </select>
                                         </div>
@@ -79,11 +83,11 @@ $_SESSION['registered'] = serialize($registeredObject);?>
                                     <div class="form-group col-lg-12">
                                         <label class="col-xs-12 col-sm-4 col-md-4 col-lg-4 control-label attributeText">Imagen de Perfil:</label>
                                         <div class="col-lg-8 inputAttribute">
-                                            <input class="form-control" type="file" name="profileImage">
+                                            <input id="image" class="form-control" type="file" name="profileImage">
                                         </div>
                                     </div>
 
-                                    <button name="update" class="button-profile pull-right btn form-button">
+                                    <button type="button" id="update-registered" name="update" class="button-profile pull-right btn form-button">
                                         <span class="glyphicon glyphicon-floppy-open"></span>
                                         Guardar Cambios
                                     </button>
@@ -116,12 +120,12 @@ $_SESSION['registered'] = serialize($registeredObject);?>
                                     Configuracion de la Cuenta
                                 </div>
 
-                                <form id="profileFormDelete" action="../controller/registeredProfileController.php" method="POST">
+                                <form id="profileFormDelete" action="../controller/deleteRegisteredController.php" method="POST">
                                     <div class="col-lg-12" id="configuracionUsuario">
                                         <p>Marca la siguiente casilla para indicar que quieres eliminar tu cuenta para siempre. Despues, pulsa el boton Eliminar</p>
-                                        <input type="checkbox" name="deleteCheckBox">
+                                        <input id="checkbox" type="checkbox" name="deleteCheckBox">
                                     Quiero eliminar mi cuenta de usuario
-                                    <button name="delete" class="btn-danger pull-right btn form-button">                                            
+                                    <button type="button" id="delete-registered" name="delete" class="btn-danger pull-right btn form-button">                                            
                                             <span class="glyphicon glyphicon-trash"></span>
                                             Eliminar
                                     </button>
