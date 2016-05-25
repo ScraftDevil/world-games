7503eae8ddef62037e47794b1a3cd36e10df2a49
darkhc <cristian14rap@gmail.com>
2016-05-25 20:41:48 +0200

    Filter Games Message ANY GAMES Show

--- view/sections/footer.php
+++ view/sections/footer.php
@@ -41,6 +41,17 @@
 <script type="text/javascript" src="../resources/js/logoutFunction.js"></script>
 
 <!--[if lt IE 9]><script src="js/html5shiv.js"></script><script src="js/respond.min.js"></script><![endif]-->
+<script>
+    String.prototype.fileExists = function() {
+        filename = this.trim();
+        var response = jQuery.ajax({
+            url: filename,
+            type: 'HEAD',
+            async: false
+        }).status;
+        return (response != "200") ? false : true;
+    }
+</script>
 <!--    SEARCH  -->
 <script type="text/javascript">
     var delay = (function(){
@@ -102,31 +113,35 @@ function search() {
                 type:  'POST',
                 typeData: 'json',
                 success:  function (json) {
-                    if (isEmptyJSON(json)) {
-                        $("#contentGames").html("No games to show!");
-                    } else {
+                    console.log(json.length);
+                    if (json.length>=3) {
                         $.each($.parseJSON(json), function() {
-                         var linesHTML = "";
-                         linesHTML += '<style>.offerOldPrice {text-decoration:line-through;}</style>';
-                         linesHTML += '<div class="col-md-3 col-sm-6 col-xs-12">';
-                         linesHTML += '<div class="gallery-item" id="Game_'+this.id+'">';
-                         linesHTML += '<div class="gallery-thumb" title="'+this.title+'"><img src="../resources/images/games/'+this.id+'.png" width="800px" height="600px" class="img-responsive" alt="'+this.title+'">';
-                         linesHTML += '<div class="image-overlay"></div>';
-                         linesHTML += '<a href="gameDetailsView.php?gameid='+this.id+'" class="gallery-zoom"><i class="fa fa-eye"></i></a>';
-                         linesHTML += '<a href="#" class="gallery-link buyItem"><i class="fa fa-shopping-cart"></i></a>';
+                            var existFilePath = "../resources/images/games/"+this.id+".png";
+                            if (!existFilePath.fileExists()) {
+                                existFilePath = "../resources/images/games/noimage.png";
+                            }
+                            var linesHTML = '<style>.offerOldPrice {text-decoration:line-through;}</style>';
+                            linesHTML += '<div class="col-md-3 col-sm-6 col-xs-12">';
+                            linesHTML += '<div class="gallery-item" id="Game_'+this.id+'">';
+                            linesHTML += '<div class="gallery-thumb" title="'+this.title+'"><img src="'+existFilePath+'" width="800px" height="600px" class="img-responsive" alt="'+this.title+'">';
+                            linesHTML += '<div class="image-overlay"></div>';
+                            linesHTML += '<a href="gameDetailsView.php?gameid='+this.id+'" class="gallery-zoom"><i class="fa fa-eye"></i></a>';
+                            linesHTML += '<a href="#" class="gallery-link buyItem"><i class="fa fa-shopping-cart"></i></a>';
+                            linesHTML += '</div>';
+                            linesHTML += '<div class="gallery-details">';
+                            linesHTML += '<div class="editContent">';
+                            linesHTML += '<h5>'+this.title+'</h5><h6><span id="price">'+this.price+' €</span></h6>';
+                            if (this.discount!=null) {
+                             linesHTML += '<h6>60 % de descuento</h6>';
+                         }
                          linesHTML += '</div>';
-                         linesHTML += '<div class="gallery-details">';
-                         linesHTML += '<div class="editContent">';
-                         linesHTML += '<h5>'+this.title+'</h5><h6><span id="price">'+this.price+' €</span></h6>';
-                         if (this.discount!=null) {
-                           linesHTML += '<h6>60 % de descuento</h6>';
-                       }
-                       linesHTML += '</div>';
-                       linesHTML += '</div>';
-                       linesHTML += '</div>'; 
-                       linesHTML += '</div>';
-                       $("#contentGames").html(linesHTML);
-                   });
+                         linesHTML += '</div>';
+                         linesHTML += '</div>'; 
+                         linesHTML += '</div>';
+                         $("#contentGames").html(linesHTML);
+                     });
+                    } else {
+                        $("#contentGames").html("<div class='col-md-3 col-sm-6 col-xs-12'>No games for show");
                     }
                 }
             });
@@ -151,12 +166,15 @@ function search() {
                 if (isEmptyJSON(json)) {
                     $("#contentGames").html("No games to show!");
                 } else {
-                    var linesHTML = '';
-                    linesHTML += '<style>.offerOldPrice {text-decoration:line-through;}</style>';
+                    var linesHTML = '<style>.offerOldPrice {text-decoration:line-through;}</style>';
                     $.each($.parseJSON(json), function() {
+                        var existFilePath = "../resources/images/games/"+this.id+".png";
+                        if (!existFilePath.fileExists()) {
+                            existFilePath = "../resources/images/games/noimage.png";
+                        }
                         linesHTML += '<div class="col-md-3 col-sm-6 col-xs-12 gallery-item-wrapper isotope-item">';
                         linesHTML += '<div class="gallery-item" id="Game_'+this.id+'">';
-                        linesHTML += '<div class="gallery-thumb" title="'+this.title+'"><img src="images/games/'+this.id+'.png" class="img-responsive" alt="'+this.title+'">';
+                        linesHTML += '<div class="gallery-thumb" title="'+this.title+'"><img src="'+existFilePath+'" class="img-responsive" alt="'+this.title+'">';
                         linesHTML += '<div class="image-overlay"></div>';
                         linesHTML += '<a href="detailsProduct.php?gameid='+this.id+'" class="gallery-zoom"><i class="fa fa-eye"></i></a>';
                         linesHTML += '<a href="#" class="gallery-link buyItem"><i class="fa fa-shopping-cart"></i></a>';
@@ -165,13 +183,13 @@ function search() {
                         linesHTML += '<div class="editContent">';
                         linesHTML += '<h5>'+this.title+'</h5><h6><span id="price">'+this.price+' €</span></h6>';
                         if (this.discount!=null) {
-                           linesHTML += '<h6>60 % de descuento</h6>';
-                       }
-                       linesHTML += '</div>';
-                       linesHTML += '</div>';
-                       linesHTML += '</div>';
-                       linesHTML += '</div>'; 
-                   });
+                         linesHTML += '<h6>60 % de descuento</h6>';
+                     }
+                     linesHTML += '</div>';
+                     linesHTML += '</div>';
+                     linesHTML += '</div>';
+                     linesHTML += '</div>'; 
+                 });
                     linesHTML += '';
                     $("#contentGames").html(linesHTML);
                     //
