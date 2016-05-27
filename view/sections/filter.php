37ab18760a48826e27827ea18d280110eaf24674
darkhc <cristian14rap@gmail.com>
2016-05-27 14:53:38 +0200

    Filter Dynamic 100%

--- view/sections/filter.php
+++ view/sections/filter.php
@@ -3,20 +3,24 @@
 		<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Genero
 		<span class="caret"></span></button>
 		<ul class="dropdown-menu">
-		  <li><a href="#" class="genreFilter" id="Aventura">Aventura</a></li>
-		  <li><a href="#" class="genreFilter" id="Accion">Accion</a></li>
-		  <li><a href="#" class="genreFilter" id="Arcade">Arcade</a></li>
-		  <li><a href="#" class="genreFilter" id="RPG">RPG</a></li>
+			<?php
+				include("../../controller/gameControllers/getGenresGamesController.php");
+				foreach($genres as $genre) {
+					echo '<li><a href="#" class="genreFilter" id="'.$genre->getName().'">'.$genre->getName().'</a></li>';
+				}
+			?>
 		</ul>
 	</div>
 	<div class="dropdown pull-left">
 		<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="hidden-xs">Plataforma</span><span class="visible-xs col-xs-12" style="padding-left:0px">Pla.</span>
 		<span class="caret"></span></button>
 		<ul class="dropdown-menu">
-		  <li><a href="#" class="platformFilter" id="Origin">Origin</a></li>
-		  <li><a href="#" class="platformFilter" id="Steam">Steam</a></li>
-		  <li><a href="#" class="platformFilter" id="Xbox">Xbox</a></li>
-		  <li><a href="#" class="platformFilter" id="PSN">PSN</a></li>
+			<?php
+				include("../../controller/gameControllers/getPlatformsGamesController.php");
+				foreach($platforms as $platform) {
+					echo ' <li><a href="#" class="platformFilter" id="'.$platform->getName().'">'.$platform->getName().'</a></li>';
+				}
+			?>
 		</ul>
 	 </div>
  </div>
