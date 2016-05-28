cf9d3ba3299b15957e57fffdaee27d44b13ed2f3
ScraftDevil <davidchicamolina@gmail.com>
2016-05-28 21:02:13 +0200

    insert registered validations admin 100%

--- admin/view/sections/footer.php
+++ admin/view/sections/footer.php
@@ -3,30 +3,6 @@
 <script type="text/javascript" src="../resources/js/calendar.js"></script>
 <script type="text/javascript" src="../resources/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="../resources/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="../resources/js/jqueryValidator-addons.js"></script>
 <script type="text/javascript" src="../resources/js/adminLogin.js"></script>
 <script type="text/javascript" src="../resources/js/adminFunctions.js"></script>
-	<script type="text/javascript">
-		
-		
-			$("#forminsert ").validate({
-				rules: {
-					title: {
-						required: "required"
-						
-
-					},
-
-					
-				},
-				messages: {
-					title: {
-						required:"El campo username esta vacio"
-						
-
-					},
-
-					
-				}
-			});
-		
-	</script>
